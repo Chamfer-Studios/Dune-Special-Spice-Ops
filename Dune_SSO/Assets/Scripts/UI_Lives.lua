be184963c633900df407c2c1d35af3a3ea5ac47c
Weedzard <arndslol@gmail.com>
2022-04-04 22:11:07 +0200

    HUD implemented

--- Dune_SSO/Assets/Scripts/UI_Lives.lua
+++ Dune_SSO/Assets/Scripts/UI_Lives.lua
@@ -1,15 +1,20 @@
 -- player = Find("Character")
 lives = 0
+path =  "Assets/HUD/slider_segments_hp3_v1.0.png"
 
 local livesIVT = INSPECTOR_VARIABLE_TYPE.INSPECTOR_INT
-local livesIV = InspectorVariable.new("lives", livesIVT, lives)
+livesIV = InspectorVariable.new("lives", livesIVT, lives)
 NewVariable(livesIV)
+
+local pathIVT = INSPECTOR_VARIABLE_TYPE.INSPECTOR_STRING
+pathIV = InspectorVariable.new("path", pathIVT, path)
+NewVariable(pathIV)
 
 -- Called each loop iteration
 function Update(dt)
 	currentLives = GetVariable("Player.lua", "lives", INSPECTOR_VARIABLE_TYPE.INSPECTOR_INT)
 	if (currentLives == lives) then
-		gameObject:GetImage():SetTexture("Assets/Textures/Blue.png") -- It would be nice if it worked with events instead of every frame
+		gameObject:GetImage():SetTexture(path) -- It would be nice if it worked with events instead of every frame
 	end
 end
 
