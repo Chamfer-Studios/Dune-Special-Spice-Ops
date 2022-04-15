7363c59875076090a491ebd0ced918712070d04f
Omicrxn <alexavila0128@gmail.com>
2022-04-15 15:46:00 +0200

    Alpha 1 Build

--- Dune_SSO/Assets/Scripts/UI_Lives.lua
+++ Dune_SSO/Assets/Scripts/UI_Lives.lua
@@ -12,9 +12,24 @@ NewVariable(pathIV)
 
 -- Called each loop iteration
 function Update(dt)
-	currentLives = GetVariable("Player.lua", "lives", INSPECTOR_VARIABLE_TYPE.INSPECTOR_INT)
-	if (currentLives == lives) then
-		gameObject:GetImage():SetTexture(path) -- It would be nice if it worked with events instead of every frame
+	if (GetVariable("GameState.lua", "characterSelected", INSPECTOR_VARIABLE_TYPE.INSPECTOR_INT) == 1) then
+	print("zhib")
+		currentLives = GetVariable("TemporaryPlayer.lua", "lives", INSPECTOR_VARIABLE_TYPE.INSPECTOR_INT)
+		if (currentLives == lives) then
+			gameObject:GetImage():SetTexture(path) -- It would be nice if it worked with events instead of every frame
+		end
+	end
+	if (GetVariable("GameState.lua", "characterSelected", INSPECTOR_VARIABLE_TYPE.INSPECTOR_INT) == 2) then
+		currentLives = GetVariable("TemporaryPlayer1.lua", "lives", INSPECTOR_VARIABLE_TYPE.INSPECTOR_INT)
+		if (currentLives == lives) then
+			gameObject:GetImage():SetTexture(path) -- It would be nice if it worked with events instead of every frame
+		end
+	end
+	if (GetVariable("GameState.lua", "characterSelected", INSPECTOR_VARIABLE_TYPE.INSPECTOR_INT) == 3) then
+		currentLives = GetVariable("TemporaryPlayer2.lua", "lives", INSPECTOR_VARIABLE_TYPE.INSPECTOR_INT)
+		if (currentLives == lives) then
+			gameObject:GetImage():SetTexture(path) -- It would be nice if it worked with events instead of every frame
+		end
 	end
 end
 
