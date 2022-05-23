63f4f53d426c58bb99b31221d19abac2ac63ed79
Omicrxn <alexavila0128@gmail.com>
2022-05-23 18:33:29 +0200

    New HUD finished

--- Dune_SSO/Assets/Scripts/New HUD Scripts/hud_selected_character.lua
+++ Dune_SSO/Assets/Scripts/New HUD Scripts/hud_selected_character.lua
@@ -1,4 +1,6 @@
-function Update(dt)
+currentCharacterId = 1
+
+function Start()
     currentCharacterId = GetVariable("GameState.lua", "characterSelected", INSPECTOR_VARIABLE_TYPE.INSPECTOR_INT)
     neralaImage = Find("Nerala Image")
     zhibImage = Find("Zhib Image")
@@ -12,6 +14,14 @@ function Update(dt)
     activeMaskQ = Find("Skill Mask Active Q")
     activeMaskW = Find("Skill Mask Active W")
     activeMaskE = Find("Skill Mask Active E")
+    hpFill1 = Find("HP Fill 1")
+    hpFill2 = Find("HP Fill 2")
+    hpFill3 = Find("HP Fill 3")
+end
+function Update(dt)
+    currentCharacterId = GetVariable("GameState.lua", "characterSelected", INSPECTOR_VARIABLE_TYPE.INSPECTOR_INT)
+ 
+
     if currentCharacterId == 1 then -- zhib
         zhibImage:GetTransform2D():SetPosition(float2.new(210, 158)) -- center position
         zhibImage:GetTransform2D():SetSize(float2.new(156.25, 156.25)) -- big size
@@ -47,13 +57,12 @@ function Update(dt)
 end
 
 function EventHandler(key, fields)
+
+
     if key == "Player_Ability" then -- fields[1] -> characterID; fields[2] -> ability n; fields[3] -> ability state
         characterID = fields[1] -- 1: zhib, 2: Nerala, 3: Omozra
         ability = fields[2] -- 0: canceled, 1: Q, 2: W, 3: E
         abilityState = fields[3] -- 1: normal, 2: active, 3: cooldown
-        Log("characterID " .. tostring(fields[1]) .. "\n")
-        Log("ability " .. tostring(fields[2]) .. "\n")
-        Log("state " .. tostring(fields[3]) .. "\n")
         if ability == 1 then
             if abilityState == 1 then
                 cooldownMaskQ.active = false
@@ -86,6 +95,31 @@ function EventHandler(key, fields)
             elseif abilityState == 3 then
                 cooldownMaskE.active = true
                 activeMaskE.active = false
+            end
+        end
+    elseif key == "Player_Health" then
+       -- fields[1] = characterID
+       -- fields[2] = currentHP
+        --fields[3] = maxHP
+        Log("Fields "..tostring(fields[1]).."\n")
+        Log("cID "..tostring(currentCharacterId).."\n")
+        if currentCharacterId == fields[1] then
+            if fields[2] == 0 then
+                hpFill1.active = false
+                hpFill2.active = false
+                hpFill3.active = false
+            elseif fields[2] == 1 then
+                hpFill1.active = true
+                hpFill2.active = false
+                hpFill3.active = false
+            elseif fields[2] == 2 then
+                hpFill1.active = true
+                hpFill2.active = true
+                hpFill3.active = false
+            elseif fields[2] == 3 then
+                hpFill1.active = true
+                hpFill2.active = true
+                hpFill3.active = true
             end
         end
     end
