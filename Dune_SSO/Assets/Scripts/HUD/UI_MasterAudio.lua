601323913c2d18d4eaa7d82fbd55a68367876b3f
ihedud <irene.hepo@gmail.com>
2022-06-07 20:43:13 +0200

    Master Audio Hell Yeah

--- Dune_SSO/Assets/Scripts/HUD/UI_MasterAudio.lua
+++ Dune_SSO/Assets/Scripts/HUD/UI_MasterAudio.lua
@@ -16,9 +16,21 @@ NewVariable(leftPosIV)
 function Update(dt)
 	if (gameObject.active == true) then
 		if (isStarting == true) then
-			print("here")
-			masterVolume = GetAudio().GetListenerVolume()
-			print("masterVolume: " .. masterVolume)
+			audio = GetAudio()
+			masterVolume = audio:GetListenerVolume()
+			masterVolume = masterVolume * 4
+			if (masterVolume == 0.0) then
+				mouseX = leftPos
+			elseif (masterVolume == 1.0) then
+				mouseX = leftPos + onePart
+			elseif (masterVolume == 2.0) then
+				mouseX = leftPos + (onePart * 2)
+			elseif (masterVolume == 3.0) then
+				mouseX = leftPos + (onePart * 3)
+			elseif (masterVolume == 4.0) then
+				mouseX = rightPos
+			end
+			gameObject:GetTransform2D():SetPositionX(mouseX)
 			isStarting = false
 		end
 		mouseX = gameObject:GetButton():GetMouseX()
@@ -34,18 +46,23 @@ function Update(dt)
 			if (mouseX < leftPos) then
 				mouseX = leftPos
 				gameObject:GetTransform2D():SetPositionX(mouseX)
+				audio:SetListenerVolume(0, 4)
 			elseif (mouseX >= (leftPos + onePart) and mouseX <= (leftPos + (onePart * 2))) then
 				mouseX = leftPos + onePart
 				gameObject:GetTransform2D():SetPositionX(mouseX)
+				audio:SetListenerVolume(1, 4)
 			elseif (mouseX >= (leftPos + (onePart * 2)) and (mouseX <= (leftPos + (onePart * 3)))) then
 				mouseX = leftPos + (onePart * 2)
 				gameObject:GetTransform2D():SetPositionX(mouseX)
+				audio:SetListenerVolume(2, 4)
 			elseif (mouseX >= (leftPos + (onePart * 3)) and (mouseX < rightPos)) then
 				mouseX = leftPos + (onePart * 3)
 				gameObject:GetTransform2D():SetPositionX(mouseX)
+				audio:SetListenerVolume(3, 4)
 			elseif (mouseX > rightPos) then
 				mouseX = rightPos
 				gameObject:GetTransform2D():SetPositionX(mouseX)
+				audio:SetListenerVolume(4, 4)
 			end
 		end
 	end
