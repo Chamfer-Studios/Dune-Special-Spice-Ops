e9c074882e7bd74baed9512d4d160282966eeb44
Montuuh <dmontufo@gmail.com>
2022-06-10 03:44:30 +0200

    Settings panel not open by default. Settings button working.

--- Dune_SSO/Assets/Scripts/HUD/UI_OpenSettingsPopUp.lua
+++ Dune_SSO/Assets/Scripts/HUD/UI_OpenSettingsPopUp.lua
@@ -1,25 +1,21 @@
 name = "Settings Menu"
-isStarting = true
 popUp = false
 
 local nameIVT = INSPECTOR_VARIABLE_TYPE.INSPECTOR_STRING
 nameIV = InspectorVariable.new("name", nameIVT, name)
 NewVariable(nameIV)
 
+function Start()
+    child = Find(name)
+    child:Active(false)
+end
+
 -- Called each loop iteration
 function UpdateUI(dt)
-	if (isStarting == true) then
-		child = gameObject:GetParent():GetParent():GetParent():GetChild(name)
-		child:Active(false)
-		isStarting = false
-	end
-	if (gameObject:GetButton():IsPressed()) then
-		if (popUp == false) then
-			popUp = true
-			child:Active(true)
-			gameObject:GetParent():GetChild("Background"):Active(false)
-		end
-	end
+    if (gameObject:GetButton():IsPressed()) then
+        popUp = not popUp
+        child:Active(popUp)
+    end
 end
 
 print("UI_OpenSettingsPopUp.lua compiled succesfully")
